df8913da00f237559f72c11d4b43e7af2668f0bd
tatsumin39 <tatsumi39@gmail.com>
2024-08-13 17:29:39 +0900

    Remove axios from dependencies as it is no longer used

--- src/utils/isUrlAccessible.js
+++ src/utils/isUrlAccessible.js
@@ -1,9 +1,12 @@
-import axios from 'axios';
+import fetch from 'node-fetch';
 
 // URLがアクセス可能かどうかを確認する関数
 export const isUrlAccessible = async (url) => {
   try {
-    await axios.get(url);
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
     return true;
   } catch (error) {
     console.error(`⛔️ URLがアクセスできませんでした - エラーメッセージ: ${error.message}`);
